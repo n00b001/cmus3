75c24e68431bbfa25138fc03c2bb6bd8a26cadc8
alex fanthome <alexanderfanthome@gmail.com>
2018-01-07 01:05:01 +0000

    More progress - seems like createdat/updatedat isn't created automatically?

--- src/main/java/com/yachtmafia/db/DBWrapperImpl.java
+++ src/main/java/com/yachtmafia/db/DBWrapperImpl.java
@@ -3,7 +3,6 @@ package com.yachtmafia.db;
 import com.yachtmafia.config.Config;
 import com.yachtmafia.messages.SwapMessage;
 import org.apache.log4j.Logger;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.sql.*;
 
@@ -20,52 +19,202 @@ public class DBWrapperImpl implements DBWrapper {
         this.config = config;
     }
 
-    @Override
-    public String getPublicAddress(String user, String coin) {
-        String query =
-                "SELECT " +
-                "   publickey " +
-                "FROM " +
-                "    wallets W INNER JOIN users U ON W.userID = U.userID " +
-                "    INNER JOIN currencies C ON C.currencyID = W.currencyID " +
-                "WHERE " +
-                "    U.email = @email AND C.symbol = @symbol";
-        try(Connection con= DriverManager.getConnection(
-                config.connectionString,
-                config.username, config.password);
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery(query);
-        ){
-            if (rs.wasNull()){
-                return null;
-            }
-            String string = rs.getString(1);
-//            while(rs.next()){
-//            }
-        } catch (SQLException e) {
-            LOG.error("Caught: ", e);
-            return null;
-        }
-//        return null;
-    }
 
     @Override
     public boolean addNewWallet(String user, String coin, String publicAddress, String privateAddress) {
-        throw new NotImplementedException();
+        String query =
+                "SELECT " +
+                        "    currencyId " +
+                        "FROM " +
+                        "    currencies " +
+                        "WHERE " +
+                        "    symbol = '" + coin + "'";
+        String currencyID = getSingleQueryString(query);
+
+        query =
+                "SELECT " +
+                        "    userId " +
+                        "FROM " +
+                        "    users " +
+                        "WHERE " +
+                        "    email = '" + user + "'";
+        String userId = getSingleQueryString(query);
+
+//        String amount = String.valueOf(message.getAmountOfCoin());
+
+
+//        query =
+//                "SELECT " +
+//                "    SUM(amount) " +
+//                "FROM "+
+//                "    portfoliobalances " +
+//                "WHERE "+
+//                "    userId = '" + userId + "' AND currencyId = '" + currencyID + "'";
+//        String runningTotal = getSingleQueryString(query);
+
+//        String leftSide = getSingleQueryString(query);
+//        String rightSide = getSingleQueryString(query);
+
+        query =
+                "INSERT INTO " +
+                        "    wallets " +
+                        "    (currencyId, userId, publickey) " +
+                        "VALUES " +
+                        "    ('" + currencyID + "', '" + userId + "', '" + publicAddress + "') ";
+        try(Connection con= DriverManager.getConnection(
+                config.connectionString,
+                config.username, config.password);
+            Statement stmt = con.createStatement()
+        ){
+            int rs = stmt.executeUpdate(query);
+            LOG.info(rs);
+//            rs.close();
+        } catch (SQLException e) {
+            LOG.error("Caught: ", e);
+            return false;
+        }
+
+        String walletId = "";
+
+        query =
+                "INSERT INTO " +
+                        "    privatekeys " +
+                        "    (walletId, privKey) " +
+                        "VALUES " +
+                        "    ('" + walletId + "', '" + privateAddress + "') ";
+        try (Connection con = DriverManager.getConnection(
+                config.connectionString,
+                config.username, config.password);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)
+        ) {
+            rs.close();
+        } catch (SQLException e) {
+            LOG.error("Caught: ", e);
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean addPortfolioBalance(SwapMessage message, long purchasedAmount) {
-        throw new NotImplementedException();
+        boolean success = true;
+        String query =
+                "SELECT " +
+                        "    currencyId " +
+                        "FROM " +
+                        "    currencies " +
+                        "WHERE " +
+                        "    symbol = '" + message.getFromCoinName() + "'";
+        String currencyID = getSingleQueryString(query);
+
+        query =
+                "SELECT " +
+                        "    userId " +
+                        "FROM " +
+                        "    users " +
+                        "WHERE " +
+                        "    email = '" + message.getUsername() + "'";
+        String userId = getSingleQueryString(query);
+
+        String amount = String.valueOf(message.getAmountOfCoin());
+
+        query =
+                "SELECT " +
+                        "    SUM(amount) " +
+                        "FROM " +
+                        "    portfoliobalances " +
+                        "WHERE " +
+                        "    userId = '" + userId + "' AND currencyId = '" + currencyID + "'";
+        String runningTotal = getSingleQueryString(query);
+
+//        String leftSide = getSingleQueryString(query);
+//        String rightSide = getSingleQueryString(query);
+
+        query =
+                "INSERT INTO" +
+                        "    portfoliobalances" +
+                        "    (currencyId, userId, amount, runningTotal)" +
+                        "VALUES" +
+                        "    ('" + currencyID + "', '" + userId + "', '" + amount + "', '" + runningTotal + "')";
+        try (Connection con = DriverManager.getConnection(
+                config.connectionString,
+                config.username, config.password);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)
+        ) {
+            rs.close();
+        } catch (SQLException e) {
+            LOG.error("Caught: ", e);
+            success = false;
+        }
+        return success;
     }
 
     @Override
     public double getFunds(String user, String coin) {
-        throw new NotImplementedException();
+        String query =
+                "SELECT " +
+                        "   runningTotal " +
+                        "FROM " +
+                        "    portfoliobalances P INNER JOIN users U ON P.userID = U.userID " +
+                        "    INNER JOIN currencies C ON C.currencyID = P.currencyID " +
+                        "WHERE " +
+                        "    U.email = '" + user + "' AND C.symbol = '" + coin + "'";
+        String funds = getSingleQueryString(query);
+        if (funds != null) {
+            return Double.parseDouble(funds);
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public String getPrivateKey(String user, String coin) {
-        throw new NotImplementedException();
+        String query =
+                "SELECT " +
+                        "   privKey " +
+                        "FROM " +
+                        "    privatekeys P " +
+                        "    INNER JOIN wallets W ON P.walletId = W.walletId " +
+                        "    INNER JOIN users U ON W.userID = U.userID " +
+                        "WHERE " +
+                        "    U.email = '" + user + "' AND C.symbol = '" + coin + "'";
+        return getSingleQueryString(query);
+    }
+
+    @Override
+    public String getPublicAddress(String user, String coin) {
+        String query =
+                "SELECT " +
+                        "   publickey " +
+                        "FROM " +
+                        "    wallets W INNER JOIN users U ON W.userID = U.userID " +
+                        "    INNER JOIN currencies C ON C.currencyID = W.currencyID " +
+                        "WHERE " +
+                        "    U.email = '" + user + "' AND C.symbol = '" + coin + "'";
+        return getSingleQueryString(query);
+    }
+
+    private String getSingleQueryString(String query) {
+        try (Connection con = DriverManager.getConnection(
+                config.connectionString,
+                config.username, config.password);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)
+        ) {
+            if (rs.next()) {
+                String string = rs.getString(1);
+                if (rs.next()) {
+                    LOG.error("More than one element was found for query: " + query);
+                }
+                return string;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            LOG.error("Caught: ", e);
+        }
+        return null;
     }
 }
---
+++ src/test/java/com/yachtmafia/kafka/ConsumerTest.java
@@ -0,0 +1,38 @@
+package com.yachtmafia.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by xfant on 2018-01-04.
+ */
+public class ConsumerTest {
+    Consumer consumer;
+
+    @Test
+    public void run() throws Exception {
+        consumer = new Consumer();
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "35.197.252.186:9092");
+        // This is the ID of this consumer machine
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "1");
+
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        //string inputs and outputs
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+
+
+        consumer.configure(props, null);
+        consumer.subscribe(Arrays.asList("my-topic"));
+        consumer.run();
+    }
+
+}
